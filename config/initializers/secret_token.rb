8f2f53e81c54d36795e918ca9a300939a35fb8c8
Meredith Underell <m.underell@gmail.com>
2013-07-11 14:29:54 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FavQuestions::Application.config.secret_token = '76b9feeeeaaa2a9c66658a25cb3de0db7a067e890af2b9f5a399c6040e0d3528d4f0a4a4b1cbcb6a17511810e5f97c6cbb4cdfc5898b84e10d20ba8cdff67b55'
